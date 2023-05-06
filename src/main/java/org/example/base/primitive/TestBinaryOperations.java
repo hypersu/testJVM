8a196f25dfb7678b64537b223114250281bb29d0
hypersu <623661487@qq.com>
2023-05-06 08:57:08 +0800

    测试二元运算

---
+++ src/main/java/org/example/base/primitive/TestBinaryOperations.java
@@ -0,0 +1,12 @@
+package org.example.base.primitive;
+
+public class TestBinaryOperations {
+
+    public static void main(String[] args) {
+        int i=0;
+        System.out.println(i++);
+
+        int i1=0;
+        System.out.println(++i1);
+    }
+}
